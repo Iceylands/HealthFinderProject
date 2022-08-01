6f20823ec8786ae78cc26d8c852d72033d0b0f86
Iceylands <rey1111333@gmail.com>
2022-08-01 12:06:37 -0500

    Completed search api functionality.

--- Backend/HealthFinder/HealthFinder/Controllers/ResultsController.cs
+++ Backend/HealthFinder/HealthFinder/Controllers/ResultsController.cs
@@ -22,7 +22,11 @@ namespace HealthFinder.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> Search(string search)
         {
-            var result_spec = _docDB.Doctors.Where(s => s.Specialty.Contains(search));
+            char[] delimiterChars = { ',', ';'};
+            //The search string should be "Specialty;Location(state);Language(on top of english)
+            string[] words = search.Split(delimiterChars);
+
+            var result_spec = _docDB.Doctors.Where(s => s.Specialty.Contains(words[0]));
             result_spec = result_spec.OrderByDescending(r => r.Rating);
             var result = result_spec;
             if (result.Count() < 3)
@@ -32,7 +36,10 @@ namespace HealthFinder.Controllers
                 result = result.Concat(resulttemp);
             }
 
-            result = result.OrderBy(s => !s.Specialty.Contains(search)).ThenByDescending(r => r.Rating);
+            result = result.OrderBy(s => !s.Specialty.Contains(words[0]))
+                .ThenBy(lang => !lang.Languages.Contains(words[2]))
+                .ThenBy(loc => !loc.Location.Contains(words[1]))
+                .ThenByDescending(r => r.Rating);
 
             return result == null ? NotFound() : Ok(result);
         }
